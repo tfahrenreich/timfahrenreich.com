541021229cc05f5f05dcc4e1d12783369f4f7ceb
tfahrenreich <t.fahrenreich@gmail.com>
2015-09-18 17:33:06 -0700

    Color Effects

--- includes/js/scripts.js
+++ includes/js/scripts.js
@@ -1,8 +1,13 @@
 $(function(){
     window.Tfahrenreich = {
         name : 'Tfahrenreich',
-        version : '1.0',
+        version : '1.1',
+        options: {
+            addPizza: false,
+            seizure: false
+        },
         init : function(){
+            window._T = Tfahrenreich;
             this.windowResize.init();
             this.onScroll.init();
         },
@@ -17,7 +22,7 @@ $(function(){
         windowResize: {
             init : function(){
                 window.onresize =  function(){
-                    var functions = $.map(Tfahrenreich.windowResize, function(value, index){
+                    var functions = $.map(_T.windowResize, function(value, index){
                         return value;
                     });
                     for (i = 1; i < functions.length; i++) {
@@ -30,7 +35,7 @@ $(function(){
                 /**Sets <sections>'s min-height to viewport height for full frame sections*/
                 var $target = $('section');
                 if($target.length > 0){
-                    var $windowHeight = Tfahrenreich.helpers.getHeight();
+                    var $windowHeight = _T.helpers.getHeight();
                     //console.log($windowHeight);
                     $target.css('min-height', $windowHeight+'px');
                 }
@@ -48,26 +53,51 @@ $(function(){
                 });
                 $(window).scroll();
             },
-            scrollfunc : function(){
-                $('section').each(function(){
-                    var randomColor = Math.floor(Math.random()*16777215).toString(16);
-                    $(this).css('background', "#"+randomColor);
-                })
+            seizure : function(){
+                if(_T.options.seizure == true){
+                    $('section').each(function(){
+                        var randomColor = Math.floor(Math.random()*16777215).toString(16);
+                        $(this).css('background', "#"+randomColor);
+                    })
+                }
             },
             addPizza : function(){
-                Tfahrenreich._data.pizzaCount++;
-                var x = Math.floor(Math.random() * Tfahrenreich.helpers.getWidth());
-                var y = Math.floor(Math.random() * Tfahrenreich.helpers.getHeight());
-                console.log('x:'+x+'y:'+y+' pizzacount:'+Tfahrenreich._data.pizzaCount);
-                $('body').append("<img src='http://a.deviantart.net/avatars/p/i/pizza-in-flames.gif?1' style='position: fixed; top:" + y + "px;left:"+ x +"px;'>");
+                if(_T.options.addPizza == true){
+                    if(_T._data.pizzaCount == undefined){_T._data.pizzaCount = 1}else{_T._data.pizzaCount++}
+                    var x = Math.floor(Math.random() * _T.helpers.getWidth());
+                    var y = Math.floor(Math.random() * _T.helpers.getHeight());
+                    $('body').append("<img src='http://a.deviantart.net/avatars/p/i/pizza-in-flames.gif?1' style='position: fixed; top:" + y + "px;left:"+ x +"px;'>");
+                }
+
             }
         },
-        _data:{
-            pizzaCount: 0
-        }
+        _data:{}
     };
 });
 
 $(document).ready(function(){
     Tfahrenreich.init()
+});
+
+$(function(){
+    function letterAnimation(letter){
+        var animationTime = (Math.round(Math.random()*5 * 10)/10);
+        console.log(letter);
+        $(letter).css({ 'transition' : animationTime+'s ease'});
+        setInterval(function(){
+            $(letter).css('color', '#'+Math.floor(Math.random()*16777215).toString(16));
+        }, 2000)
+    }
+
+    $('.letter-container').each(function(){
+        var chars = $(this).text().split('');
+        var $thisContainer = $(this);
+        $thisContainer.empty();
+        $.each(chars,function(i,l){
+            var appendix = $('<span>'+l+'</span>');
+            $thisContainer.append(appendix);
+            letterAnimation(appendix);
+        });
+    });
+
 });
--- templates/index/home.tpl
+++ templates/index/home.tpl
@@ -1,12 +1,3 @@
-<section data-resize>
-stuff
-</section>
-<section data-resize>
-stuff
-</section>
-<section data-resize>
-stuff
-</section>
-<section data-resize>
-stuff
+<section id="home-slide" data-resize>
+    <h1><span class="letter-container">Tim Fahrenreich</span><small><span class="letter-container">front end developer + designer</span></small></h1>
 </section>
