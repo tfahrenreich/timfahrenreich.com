affe160bdcedca220948eeae13ec3a410c0286b9
tfahrenreich <t.fahrenreich@gmail.com>
2015-09-25 02:45:28 -0700

    Optimize JS

--- includes/js/scripts.js
+++ includes/js/scripts.js
@@ -1,28 +1,28 @@
-$(function(){
+!function () {
     window.Tfahrenreich = {
-        name : 'Tfahrenreich',
-        version : '1.1',
+        name: 'Tfahrenreich',
+        version: '1.3',
         options: {
             addPizza: false,
             seizure: false
         },
-        init : function(){
+        init: function () {
             window._T = Tfahrenreich;
             this.windowResize.init();
             this.onScroll.init();
         },
-        helpers : {
-            getWidth: function(){
+        helpers: {
+            getWidth: function () {
                 return Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
             },
-            getHeight: function(){
+            getHeight: function () {
                 return Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
             }
         },
         windowResize: {
-            init : function(){
-                window.onresize =  function(){
-                    var functions = $.map(_T.windowResize, function(value, index){
+            init: function () {
+                window.onresize = function () {
+                    var functions = $.map(_T.windowResize, function (value, index) {
                         return value;
                     });
                     for (i = 1; i < functions.length; i++) {
@@ -31,20 +31,20 @@ $(function(){
                 };
                 window.onresize();
             },
-            minHeight : function(){
+            minHeight: function () {
                 /**Sets <sections>'s min-height to viewport height for full frame sections*/
                 var $target = $('section');
-                if($target.length > 0){
+                if ($target.length > 0) {
                     var $windowHeight = _T.helpers.getHeight();
                     //console.log($windowHeight);
-                    $target.css('min-height', $windowHeight+'px');
+                    $target.css('min-height', $windowHeight + 'px');
                 }
             }
         },
         onScroll: {
-            init : function(){
-                $(window).scroll(function(){
-                    var functions = $.map(Tfahrenreich.onScroll, function(value, index){
+            init: function () {
+                $(window).scroll(function () {
+                    var functions = $.map(Tfahrenreich.onScroll, function (value, index) {
                         return value;
                     });
                     for (i = 1; i < functions.length; i++) {
@@ -53,77 +53,86 @@ $(function(){
                 });
                 $(window).scroll();
             },
-            seizure : function(){
-                if(_T.options.seizure == true){
-                    $('section').each(function(){
-                        var randomColor = Math.floor(Math.random()*16777215).toString(16);
-                        $(this).css('background', "#"+randomColor);
+            seizure: function () {
+                if (_T.options.seizure == true) {
+                    $('section').each(function () {
+                        var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+                        $(this).css('background', "#" + randomColor);
                     })
                 }
             },
-            addPizza : function(){
-                if(_T.options.addPizza == true){
-                    if(_T._data.pizzaCount == undefined){_T._data.pizzaCount = 1}else{_T._data.pizzaCount++}
+            addPizza: function () {
+                if (_T.options.addPizza == true) {
+                    if (_T._data.pizzaCount == undefined) {
+                        _T._data.pizzaCount = 1
+                    } else {
+                        _T._data.pizzaCount++
+                    }
                     var x = Math.floor(Math.random() * _T.helpers.getWidth());
                     var y = Math.floor(Math.random() * _T.helpers.getHeight());
-                    $('body').append("<img src='http://a.deviantart.net/avatars/p/i/pizza-in-flames.gif?1' style='position: fixed; top:" + y + "px;left:"+ x +"px;'>");
+                    $('body').append("<img src='http://a.deviantart.net/avatars/p/i/pizza-in-flames.gif?1' style='position: fixed; top:" + y + "px;left:" + x + "px;'>");
                 }
 
             }
         },
-        _data:{}
+        _data: {}
     };
-});
+}();
+!function () {
+    Tfahrenreich.init();
 
-$(document).ready(function(){
-    Tfahrenreich.init()
-});
+    var letters = [], randomColor, animationTime;
+    randomColor = function () {
+        var colors = ['#356565', '#006A80', '#2C5D5D', '#47C3C3', '#16AF98', '#0EA58E', '#3EBFAC', '#24B5B5', '#71B09F'];
+        return colors[Math.round(Math.random() * (9 - 1) + 1)];
+    };
+    animationTime = function () {
+        return Math.round((Math.random() * (7 - 3) + 3) * 10) / 10+ 's ease color';
+    };
 
-$(function(){
-    var colors = ['#356565', '#006A80', '#2C5D5D' , '#47C3C3', '#16AF98', '#0EA58E', '#3EBFAC' , '#24B5B5', '#71B09F' ];
-    var letters = [];
-    var randomColor = function(){
-        return colors[Math.round(Math.random() * (9-1) + 1)];
-    };
-    var animationTime = function(){
-        return (Math.round((Math.random() * (7 - 3) + 3)*10)/10);
-    };
-    $('.letter-container').each(function(){
+    $('.letter-container').each(function () {
         var chars = $(this).text().split('');
         var $thisContainer = $(this);
         $thisContainer.empty();
-        $.each(chars,function(i,l){
-            var appendix = $('<span>'+l+'</span>');
+        $.each(chars, function (i, l) {
+            var appendix = $('<span>' + l + '</span>');
             $thisContainer.append(appendix);
             letters.push(appendix);
         });
     });
-    $('#home-slide').css('border','7px solid #FFFFFF');
-    setTimeout(function(){
-        $.each(letters, function(i,l){
+    $('#home-slide').css('border', '7px solid #FFFFFF');
+    setTimeout(function () {
+        $.each(letters, function (i, l) {
             $(l).css({
-                'color'                 : '#24B5B5' ,
-                'transition'            : animationTime() + 's ease color',
-                '-moz-transition'       : animationTime() + 's ease color',
-                '-webkit-transition'    : animationTime() + 's ease color'
+                'transition': animationTime,
+                '-moz-transition': animationTime,
+                '-webkit-transition': animationTime,
+                'color': '#24B5B5'
             });
         })
     }, 0);
-    setInterval(function(){
-        $('#home-slide').css({
-            'border-top-color'      : randomColor(),
-            'border-bottom-color'   : randomColor(),
-            'border-left-color'     : randomColor(),
-            'border-right-color'    : randomColor()
-        });
-
-        $.each(letters, function(i,l){
-            $(l).css({
-                'color'                 : randomColor(),
-                'transition'            : animationTime() + 's ease color',
-                '-moz-transition'       : animationTime() + 's ease color',
-                '-webkit-transition'    : animationTime() + 's ease color'
+    !function animationLoop(i) {
+        setTimeout(function () {
+            _T._data.loopIteration = i;
+            $('#home-slide').css({
+                'border-top-color': randomColor,
+                'border-bottom-color': randomColor,
+                'border-left-color': randomColor,
+                'border-right-color': randomColor
             });
-        })
-    }, 4000);
-});
+
+            $.each(letters, function (i, l) {
+                $(l).css({
+                    'color': randomColor,
+                    'transition': animationTime,
+                    '-moz-transition': animationTime,
+                    '-webkit-transition': animationTime
+                });
+            });
+            i++;
+            animationLoop(i)
+        }, 4000);
+    }(0);
+}();
+
+
--- templates/index/home.tpl
+++ templates/index/home.tpl
@@ -1,6 +1,6 @@
 <section id="home-slide" data-resize>
     <h1>
-        <span class="letter-container">HEADER</span>
+        <span class="letter-container">Tim Fahrenreich</span>
         <small><span class="letter-container">SUB HEADER</span></small>
     </h1>
 </section>
